2d7d7c6232c297ab1c1c074dc1a773da79ed57de
salah <salahdinjemal@gmail.com>
2024-02-06 15:32:09 -0600

    add routes

--- src/routes/PageRoutes.js
+++ src/routes/PageRoutes.js
@@ -1,15 +1,18 @@
 import {Route, Routes} from "react-router-dom";
 import PropertyListing from "../scenes/property/PropertyListing";
 import Dashboard from "../scenes/Dashboard";
+import UserListing from "../scenes/user/UserListing";
+import PropertyDetail from "../scenes/property/PropertyDetail";
+import AddProperty from "../scenes/property/AddProperty";
 
 const PageRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Dashboard/>}/>
             <Route path="/properties" element={<PropertyListing/>}/>
-            <Route path="/contacts" element={<div>Contacts Page</div>}/>
-            <Route path="/invoices" element={<div>Invoices Page</div>}/>
-            <Route path="/form" element={<div>Form Page</div>}/>
+            <Route path="/users" element={<UserListing/>}/>
+            <Route path="/properties/:id" element={<PropertyDetail/>}/>
+            <Route path="/properties/add" element={<AddProperty/>}/>
             <Route path="/bar" element={<div>Bar Page</div>}/>
             <Route path="/pie" element={<div>Pie Page</div>}/>
             <Route path="/line" element={<div>Line Page</div>}/>
